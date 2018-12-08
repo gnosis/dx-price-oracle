cb881b77bd6d6f095ebbfe8bc42e5fd0cdc05eb6
dteiml <dteiml@gmail.com>
2018-12-08 13:46:34 +0100

    prototype

--- contracts/DutchXPriceOracle.sol
+++ contracts/DutchXPriceOracle.sol
@@ -5,14 +5,34 @@ contract DutchX {
 }
 
 contract DutchXPriceOracle {
+
+    struct fraction {
+        uint num;
+        uint den;
+    }
+
+    modifier onlyWhitelisted(address token) {        
+        // Require token to be whitelisted in DutchX
+        require(isWhitelisted(token), "not whitelisted");
+    }
+    
     DutchX dutchX;
     address ethToken;
     
     constructor(DutchX _dutchX, address _ethToken)
         public
     {
-        dutchX = _DutchX;
+        dutchX = _dutchX;
         ethToken = _ethToken;
+    }
+
+    function getPriceOfWhitelistedToken(address token)
+        public
+        view
+        isWhitelisted(token);
+        returns (uint num, uint den)
+    {
+        (num, den) = getPrice(token):
     }
 
     function getPrice(address token)
@@ -20,25 +40,55 @@ contract DutchXPriceOracle {
         view
         returns (uint num, uint den)
     {
-        (num, den) = getPriceFromLastNAuctions(token, 10);
-
+        (num, den) = getPriceFromLastNAuctions(token, 9);
     }
 
-    function getPriceFromLastNAuctions(address token, uint numberOfAuctions) {
-        
-    }
+    function getPriceFromLastNAuctions(address token, uint numberOfAuctions)
+        public
+        view
+        returns (uint num, uint den)
+    {
         uint latestAuctionIndex = dutchX.getAuctionIndex(token, ethToken);
-        (num, den) = getPriceInPastAuction(token, ethToken, latestAuctionIndex - 1);
 
+        fraction[] memory prices = new fraction[](numberOfAuctions);
+
+        for (uint i = numberOfAuctions - 1; i >= 0; i--) {
+            // Loop should begin by calling auction index lAI - numberOfAuctions
+            // and end by calling lAI - 1
+            // That gives numberOfAuctions calls
+            (_num, _den) = getPriceInPastAuction(token, ethToken, latestAuctionIndex - 1 - i);
+
+            // Prices are now sorted in chronological order
+            prices.push(fraction(_num, _den));
+        }
+
+        (num, den) = getMedian(prices);
     }
 
-    function isGreaterFraction(uint num1, uint den1, uint num2, uint den2)
+    function getMedian(fraction[] memory prices)
+        public
+        pure
+        returns (uint num, uint den)
+    {
+        // TODO
+        // Will use algorithm described in https://stackoverflow.com/a/28822243
+    }
+
+    function isSmallerFraction(uint num1, uint den1, uint num2, uint den2)
         public
         pure
         returns (bool)
     {
         // Possibly use floating point numbers?
 
-        return (num1 * den2 > num2 * den1);
+        return (num1 * den2 < num2 * den1);
+    }
+
+    function isWhitelisted(address token) 
+        public
+        view
+        returns (bool)
+    {
+        return dutchX.approvedTokens(token);
     }
 }
