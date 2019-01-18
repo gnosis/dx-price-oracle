9fbfa03ac1fa18187841886eadfe30745b14fb94
dteiml <dteiml@gmail.com>
2019-01-18 16:35:11 +0100

    refactor migrations

--- migrations/2_DEV.js
+++ migrations/2_DEV.js
@@ -3,7 +3,8 @@ const Mock = artifacts.require('MockContract')
 
 module.exports = async function (deployer, network, accounts) {
     if (network === 'development') {
-        const mock = await deployer.deploy(Mock)
-        await deployer.deploy(PriceOracle, mock.address, '0x0000000000000000000000000000000000000001')
+        await deployer
+            .then(() => deployer.deploy(Mock))
+            .then(() => deployer.deploy(PriceOracle, Mock.address, '0x0000000000000000000000000000000000000001'))
     }
 }
